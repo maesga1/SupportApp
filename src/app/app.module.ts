80c2a13816cca1825c5d15b9b534927611d62cc4
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-07 10:19:19 +0200

    Create component listasolicitudes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListasolicitudesComponent } from './listasolicitudes/listasolicitudes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
+    ListasolicitudesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/listasolicitudes/listasolicitudes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listasolicitudes',
+  templateUrl: './listasolicitudes.component.html',
+  styleUrls: ['./listasolicitudes.component.scss']
+})
+export class ListasolicitudesComponent {
+
+}
