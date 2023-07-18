9ee4b07385b1b4c0899a4cc9a6758c5059063bd2
Francisco Manuel <fsaa93@gmail.com>
2023-07-18 08:54:03 +0200

    backend done

--- backend/src/main/java/org/supportapp/supportapp/models/Solicitud.java
+++ backend/src/main/java/org/supportapp/supportapp/models/Solicitud.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 public class Solicitud {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
     @Column(name = "nombre")
